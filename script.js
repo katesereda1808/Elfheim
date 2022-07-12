74db912b93d84cd01196ba8cebe516957b46a384
katesereda1808 <katesereda22@gmail.com>
2022-07-12 19:07:48 +0200

    main page animated

--- script.js
+++ script.js
@@ -11,3 +11,65 @@ function closeMenu(){
     mobileMenu.classList.add('hide');
     console.log('close');
 }
+
+
+
+function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+          // устроить проверку, есть ли анимированные объекты
+          
+          // 4 класса анимаций
+
+          // если элемент содержит такой-то класс(детектор анимации), к нему добавляется такой-то класс(сама анимация)
+
+     
+        if(change.target.classList.contains('emersion_animation')){
+            change.target.classList.add('show_from_the_bottom');
+        }
+        if(change.target.classList.contains('opacity_animation')){
+            change.target.classList.add('increase_opacity')
+        }
+        
+        if(change.target.classList.contains('opacity_and_emersion_animation')){
+            change.target.classList.add('from_the_bottom_and_opacity')
+        }
+        // if(change.target.classList.contains('opacity_animation')){
+        //     change.target.classList.add('increase_opacity')
+        // }
+        
+        
+        change.target.classList.remove('hide_content');
+      }
+    });
+}
+  // button, p, form - increase_opacity
+  // h1 h2 - show_from_the_bottom
+  //
+  let options = { threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let headers = document.querySelectorAll('h1');
+  let block_headers = document.querySelectorAll('h2');
+//   if(block_headers){
+//     for (let header of block_headers) {
+//         observer.observe(header);
+//       }
+//   }
+
+  // opacity animation
+  let opacity_animation = document.querySelectorAll('.opacity_animation');
+  for(let item of opacity_animation){
+    observer.observe(item);
+  }
+  // emersion animation
+  let emersion_animation = document.querySelectorAll('.emersion_animation');
+  for(let item of emersion_animation){
+    observer.observe(item);
+  }
+
+  let opacity_and_emersion_animation = document.querySelectorAll('.opacity_and_emersion_animation');
+  for(let item of opacity_and_emersion_animation){
+    observer.observe(item);
+  }
+
+
