403e48885bb16599a27e99c37bd682a3965352ed
katesereda1808 <katesereda22@gmail.com>
2022-07-19 18:02:17 +0200

    mobile version projects page fixed

--- script.js
+++ script.js
@@ -17,28 +17,29 @@ function closeMenu(){
 function onEntry(entry) {
     entry.forEach(change => {
       if (change.isIntersecting) {
-          // устроить проверку, есть ли анимированные объекты
-          
-          // 4 класса анимаций
-
-          // если элемент содержит такой-то класс(детектор анимации), к нему добавляется такой-то класс(сама анимация)
-
-     
+          // если элемент содержит класс-детектор анимации, к нему добавляется класс анимации
         if(change.target.classList.contains('emersion_animation')){
             change.target.classList.add('show_from_the_bottom');
         }
         if(change.target.classList.contains('opacity_animation')){
             change.target.classList.add('increase_opacity')
         }
-        
         if(change.target.classList.contains('opacity_and_emersion_animation')){
             change.target.classList.add('from_the_bottom_and_opacity')
         }
-        // if(change.target.classList.contains('opacity_animation')){
-        //     change.target.classList.add('increase_opacity')
+        
+        // if(change.target.classList.contains('column')){
+        //   console.log('wood')
+        //   change.target.classList.add('wood_animation_3')
         // }
-        
-        
+        if(change.target.classList.contains('wood')){
+          console.log('wood')
+          change.target.classList.add('wood1')
+        }
+
+
+
+
         change.target.classList.remove('hide_content');
       }
     });
@@ -87,10 +88,17 @@ if(document.querySelector('.feedback_form_btn')){
 
 }
 
+function isDesktop() {
+  if(document.documentElement.clientWidth>900){
+    return true
+  } else {
+    false
+  }
+}
 
 
 
-if(document.querySelector('.project_page')){
+if(document.querySelector('.project_page_desktop')){
   let text_half = document.querySelector('.text_half');
   let img_half = document.querySelector('.img_half');
   let texts_arr = [`<div class="text_half__content text_centre hidden first_slide" id="0"><div class="chevron desktop">
@@ -98,7 +106,10 @@ if(document.querySelector('.project_page')){
 </div>
 <h2 class="block_heading">Гостиная комната</h2>
 <p class="typing_animation">Давным давно...</p>
-<p>Мы еще пишем историю для это проекта, скоро она появится на сайте!</p></div>`, `<div class="text_half__content text_centre hidden" id="1"><div class="chevron desktop">
+<p>Мы еще пишем историю для это проекта, скоро она появится на сайте!</p><div class="scroll_icon desktop">
+<img src="./assets/Icons/Scroll.svg" alt="" srcset="">
+<img class="scroll_arrow" src="./assets/Icons/Scroll_arrow.svg" alt="" srcset="">
+</div></div>`, `<div class="text_half__content text_centre hidden" id="1"><div class="chevron desktop">
 <img src="./assets/Icons/Back.svg" alt="">
 </div>
 <h2 class="block_heading">Гостиная по индивидуальному дизайн-проекту</h2>
@@ -114,9 +125,32 @@ if(document.querySelector('.project_page')){
 </div>
 <h3 class="block_heading">На каком цвете мебели остановились?</h3>
 <p>Выбрали коричневый, тёплый чёрный цвета — они отлично подчёркивают характер хозяев, наделяют помещение особым шармом. При этом не накапливают негатив и здорово сочетаются с мягкими оттенками стен, потолка и пола.</p>
-<h3>Из каких материалов решили изготавливать мебель?</h3>
+<h3 class="block_heading">Из каких материалов решили изготавливать мебель?</h3>
 <p>Панели из высококачественного шпона, эмалированный МДФ — это современные, наиболее износостойкие и неприхотливые материалы: они хорошо переносят перепады температуры, не боятся воды и не трескаются от времени. Да и уход за ними — одно удовольствие.</p></div>`];
   let img_arr = [`<img class="project_img_animation project_img" src="./assets/Images/Cat/1.jpg" alt="">`, `<img class="project_img_animation project_img" src="./assets/Images/Cat/2.jpg" alt="">`, `<img class="zoomIn zoomed project_img" src="./assets/Images/Cat/2.jpg" alt="">`, `<img class="project_img_animation project_img" src="./assets/Images/Cat/3.jpg" alt="">`];
+  if(isDesktop()){
+    addText(texts_arr, 0);
+    addImg(img_arr, 0);
+
+    let isEvent = false;
+    let project_block = document.querySelector('.project_page__block1_content');
+    project_block.addEventListener('wheel', function name(e) {
+    if(e.deltaY >0 && window.scrollY < 100){
+      e.preventDefault();
+      e.stopPropagation();
+      if(!isEvent){
+        isEvent = true;
+        setTimeout( function() {
+        let text = document.querySelector('.text_half__content');
+        let id = +text.id;
+        addText(texts_arr, (id+1));
+        addImg(img_arr, (id+1));
+          isEvent = false;
+        }, 1500 );
+      }
+    }
+  })
+  }
 
   function addText(arr, i) {
     if(!arr[i]){
@@ -133,6 +167,7 @@ if(document.querySelector('.project_page')){
         document.querySelector('.text_half__content').classList.remove('hidden')
     }, 0)
   }
+
   function addImg(arr, i){
     if(document.querySelector('.project_img_animation')){
       document.querySelector('.project_img_animation').classList.remove('project_img_animation')
@@ -146,6 +181,7 @@ if(document.querySelector('.project_page')){
     }
     img_half.innerHTML += arr[i];
   }
+
   function findSlide(arr, i){
     arr.map((item, index)=>{
         if(i === index){
@@ -153,27 +189,30 @@ if(document.querySelector('.project_page')){
         }
       });
   }
-  addText(texts_arr, 0);
-  addImg(img_arr, 0);
-  
-  let isEvent = false;
-  let project_block = document.querySelector('.project_page__block1_content');
-  console.log(project_block)
-  project_block.addEventListener('wheel', function name(e) {
-    if(e.deltaY >0 && window.scrollY < 100){
-      e.preventDefault();
-      e.stopPropagation();
-      if(!isEvent){
-        isEvent = true;
-        setTimeout( function() {
-        let text = document.querySelector('.text_half__content');
-        let id = +text.id;
-        addText(texts_arr, (id+1));
-        addImg(img_arr, (id+1));
-          isEvent = false;
-        }, 1500 );
-      }
-    }
-  }
-  )
 }
+
+let gallery = document.querySelector('.main__block5_gallery');
+if(gallery){
+  console.log(document.querySelectorAll('.project_card'))
+}
+////////////
+// function showSlides(slides, n){
+//   if (n > slides.length-1) {n = 0}    
+//   if (n < 0) {
+//     n = slides.length-1;
+//   }
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].classList.remove('shown');
+//   }
+//   slides[n].classList.add('shown');
+
+// }
+// function findSlide() {
+//   let shown = slider_content.querySelector('.shown');
+//   slidesArr = Array.from(slides);
+//   return(slidesArr.indexOf(shown));
+// }
+// chevron_left.addEventListener('click', ()=>showSlides(findSlide()-1));
+// chevron_right.addEventListener('click', ()=>showSlides(findSlide()+1));
+///////////////////
+////////////////////
